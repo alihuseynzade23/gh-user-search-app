13c3c057006fef1aedbcae7a6d20876b954d6840
Ali <86948893+AleksHNZ@users.noreply.github.com>
2024-01-01 14:49:10 +0400

    End of the project

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    login: "alihuseynzade23",
+    avatar: "https://avatars.githubusercontent.com/u/86948893?v=4",
+    name: "Ali",
+    company: "Azerbaijan Airlines",
+    blog: "https://alihuseynzade.netlify.app/",
+    location: "Baku, Azerbaijan",
+    bio: "Front-end Developer",
+    twitter: "",
+    repos: 49,
+    followers: 9,
+    following: 8,
+    created: "2021-07-05T07:04:38Z",
+};
